d8ec98a745decd5f2486edf17fb42a4241203d4b
Florian Günter <florian.guenter@stud.hs-flensburg.de>
2025-11-13 16:30:24 +0100

    Reorder definitions

--- tests/BinOpTests.hs
+++ tests/BinOpTests.hs
@@ -9,24 +9,6 @@ import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (HasCallStack, assertFailure, testCaseInfo, (@?))
 import Prelude hiding (exp)
 
-ident :: String -> Exp Parsed
-ident name = ExpName (Name dummySourceSpan (Ident dummySourceSpan name :| []))
-
-binOp :: Op -> Exp Parsed -> Exp Parsed -> Exp Parsed
-binOp = flip (BinOp dummySourceSpan)
-
-testRoundTrip :: (HasCallStack) => Exp Parsed -> TestTree
-testRoundTrip e = testCaseInfo (prettyExplicit e) $
-  case parserWithState (ParserState ParseFull False) exp "" (show (pretty e)) of
-    Left err -> assertFailure (show err)
-    Right (e', _) -> do
-      eq IgnoreSourceSpan e e' @? "pretty-printed (" ++ show (pretty e) ++ ") reparsed as " ++ prettyExplicit e'
-      pure ("pretty-printed: " ++ show (pretty e))
-
-prettyExplicit :: Exp Parsed -> String
-prettyExplicit (BinOp _ a op b) = "(" ++ prettyExplicit a ++ " " ++ show (pretty op) ++ " " ++ prettyExplicit b ++ ")"
-prettyExplicit e = show (pretty e)
-
 testBinOp :: [TestTree]
 testBinOp =
   [ testGroup
@@ -75,3 +57,21 @@ testBinOp =
     c = ident "c"
     d = ident "d"
     e = ident "e"
+
+testRoundTrip :: (HasCallStack) => Exp Parsed -> TestTree
+testRoundTrip e = testCaseInfo (prettyExplicit e) $
+  case parserWithState (ParserState ParseFull False) exp "" (show (pretty e)) of
+    Left err -> assertFailure (show err)
+    Right (e', _) -> do
+      eq IgnoreSourceSpan e e' @? "pretty-printed (" ++ show (pretty e) ++ ") reparsed as " ++ prettyExplicit e'
+      pure ("pretty-printed: " ++ show (pretty e))
+
+prettyExplicit :: Exp Parsed -> String
+prettyExplicit (BinOp _ a op b) = "(" ++ prettyExplicit a ++ " " ++ show (pretty op) ++ " " ++ prettyExplicit b ++ ")"
+prettyExplicit e = show (pretty e)
+
+binOp :: Op -> Exp Parsed -> Exp Parsed -> Exp Parsed
+binOp = flip (BinOp dummySourceSpan)
+
+ident :: String -> Exp Parsed
+ident name = ExpName (Name dummySourceSpan (Ident dummySourceSpan name :| []))
